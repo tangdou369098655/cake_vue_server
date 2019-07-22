779cbf6bef6d6ec95d52ac84564dc938c17bd382
tangdou369098655 <369098655@qq.com>
2019-07-22 22:20:25 +0800

    7-22

--- routers/index.js
+++ routers/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../pool");
 
-//需要查询多个数据表格
+//一次查询返回4条数据
 router.get("/", (req, res) => {
   var status = req.query.status;
   var output = {
@@ -23,5 +23,27 @@ router.get("/", (req, res) => {
     console.log(444)
   }
 })
+
+//一次查询返回多条数据
+router.get("/insale", (req, res) => {
+  var status = req.query.insale;
+  var output = {
+    product: {}
+  }
+  if (status !== undefined) {
+    var sql1 = `select * from cake_index_product where index_sale_new=?`;
+    pool.query(sql1, [status], (err, result) => {
+      if (err) console.log(err);
+      output.product = result;
+          res.send(output);
+      })
+    
+  }else{
+    res.send(output);
+    console.log(444)
+  }
+})
+
+
 
 module.exports = router;
